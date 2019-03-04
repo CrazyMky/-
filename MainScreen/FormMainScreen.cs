9b4947a3b82480c213ece1775e1c0d61755ff927
Batraz_600_001 <batik611@yandex.ru>
2019-03-04 14:38:35 +0300

    First commit

---
+++ MainScreen/FormMainScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainScreen
+{
+    public partial class FormMainScreen : Form
+    {
+        public FormMainScreen()
+        {
+            InitializeComponent();
+            showPicture();
+        }
+        void showPicture()
+        {
+            pictureBox1.Image = Image.FromFile("images/1.jpg");
+            pictureBox2.Image = Image.FromFile("images/2.jpg");
+            pictureBox3.Image = Image.FromFile("images/3.jpg");
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
